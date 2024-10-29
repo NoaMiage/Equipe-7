c0cab7b6daa73e4400e951b7581545bf9e121f5a
Noa <noa.12v@icloud.com>
2024-10-29 22:15:45 +0100

    variante 1 test

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -18,8 +18,13 @@ TEMP_PREVISION=$(curl -s "wttr.in/$VILLE?format=3" | grep -Eo '[-+]\d+°C')
 
 DATE=$(date +"%Y-%m-%d")
 HEURE=$(date +"%H:%M")
+#j'ajoute la variante 1 pour savoir le vent, l'humidité et la visibilité de la ville
+VENT=$(grep -Eo '[0-9]+ km/h' meteo_temp.txt | head -n 1)
+HUMIDITE=$(grep -Eo '[0-9]+%' meteo_temp.txt | head -n 1)
+VISIBILITE=$(grep -Eo '[0-9]+ km' meteo_temp.txt | head -n 1)
 
-echo "$DATE - $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_PREVISION" >> $FICHIER
+#je rajoute les nouvelles données dans le echo 
+echo "$DATE - $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_PREVISION - $VENT - $HUMIDITE - $VISIBILITE" >> $FICHIER
 echo "Les données météo ont été enregistrées dans $FICHIER"
 
-echo "$DATE - $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_PREVISION" >> "$HISTORIQUE_FICHIER"
+echo "$DATE - $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_PREVISION- $VENT - $HUMIDITE - $VISIBILITE" >> "$HISTORIQUE_FICHIER"
