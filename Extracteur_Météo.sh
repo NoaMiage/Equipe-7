b199ceea0d6f9d860991edfe05c61b5d89c1cfa7
Noa <noa.12v@icloud.com>
2024-10-29 09:52:37 +0100

    changement du grep car le -P n'est pas compatible avec tout les ordinateurs ( cela n'affichait pas les données sur MACOS)

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -13,8 +13,8 @@ FICHIER="meteo.txt"
 
 curl -s "wttr.in/$VILLE?format=%C+%t" > meteo_temp.txt
 
-TEMP_ACTUELLE=$(grep -oP '[-+]\d+°C' meteo_temp.txt | head -n 1)
-TEMP_PREVISION=$(curl -s "wttr.in/$VILLE?format=3" | grep -oP '[-+]\d+°C')
+TEMP_ACTUELLE=$(grep -Eo '[-+]\d+°C' meteo_temp.txt | head -n 1)
+TEMP_PREVISION=$(curl -s "wttr.in/$VILLE?format=3" | grep -Eo '[-+]\d+°C')
 
 DATE=$(date +"%Y-%m-%d")
 HEURE=$(date +"%H:%M")
@@ -22,4 +22,4 @@ HEURE=$(date +"%H:%M")
 echo "$DATE - $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_PREVISION" >> $FICHIER
 echo "Les données météo ont été enregistrées dans $FICHIER"
 
-echo "$DATE - $HEURE - $CITY : $TEMP_ACTUELLE - $TEMP_PREVISION" >> "$HISTORIQUE_FICHIER"
+echo "$DATE - $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_PREVISION" >> "$HISTORIQUE_FICHIER"
